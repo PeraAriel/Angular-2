234859e9f354386e6cac5918b42fb337bab9ee8c
PeraAriel <159137323+PeraAriel@users.noreply.github.com>
2025-05-08 13:45:20 +0200

    Commit Finale
    Completamento del progetto da parte di Papini Federico

--- product-list-app/src/app/app.component.spec.ts
+++ product-list-app/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'product-app' title`, () => {
+  it(`should have the 'product-list-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('product-app');
+    expect(app.title).toEqual('product-list-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, product-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, product-list-app');
   });
 });
